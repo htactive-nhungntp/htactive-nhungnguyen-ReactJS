233f7b48f06de23b493a4a1392177f3cebb4dae3
htactive-nhungntp <nhungntp@htactive.com>
2019-07-02 15:19:23 +0700

    Demo State

--- learningReact/demo_react/src/App.js
+++ learningReact/demo_react/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import DemoProps from "./Props/DemoProps";
+import DemoClock from "./StatesLifecycle/DemoClock";
 function App() {
   const student = { id: 1, name: "nhung", age: 20 };
   const menu = {
@@ -16,6 +17,7 @@ function App() {
       <Content />
       <Footer />
       <DemoProps student={student} />
+      <DemoClock />
     </>
   );
 }
