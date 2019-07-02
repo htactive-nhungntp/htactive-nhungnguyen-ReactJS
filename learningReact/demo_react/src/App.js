dc633fcf41eeec59f1b37fbf90e2b8cba0c16334
htactive-nhungntp <nhungntp@htactive.com>
2019-07-02 14:57:27 +0700

    Update the third times part of destructuring in es6

--- learningReact/demo_react/src/App.js
+++ learningReact/demo_react/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-// import logo from './logo.svg';
 import "./App.css";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import DemoProps from "./Props/DemoProps";
-
 function App() {
   const student = { id: 1, name: "nhung", age: 20 };
   const menu = {
