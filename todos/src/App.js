802437dafb42cee51cf5508f6c07f21976ac0d42
htactive-nhungntp <nhungntp@htactive.com>
2019-07-04 09:34:54 +0700

    Update todolist

---
+++ todos/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Content from "./Components/Content";
+import Item from "./Components/Item";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Item />
+    </>
+  );
+}
+
+export default App;
